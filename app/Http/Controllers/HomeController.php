5dcd301f644f2a01f45939ecda7541554c64e13e
Shikanto <antonello.anzini.comunicazioni@gmail.com>
2022-01-13 10:19:12 +0100

    change return view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Comics;
 class HomeController extends Controller
 {
     function index(){
-        
+        $comics= Comics::all();
+        //dump($comics);
+
+        return view("comics.index", compact('comics'));
     }
 }
